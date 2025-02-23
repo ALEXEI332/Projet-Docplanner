28c62cd77cbe2b6cfa7f5f829feb9801df2d5a77
ALEXEI332 <ztim908@gmail.com>
2025-02-23 16:09:27 +0000

    Third commit

---
+++ MonProjet/script.js
@@ -0,0 +1,24 @@
+
+document.getElementById("nikeshoes-1").addEventListener("mouseover", () => {
+    document.getElementById("img-display-on").src = "https://static.nike.com/a/images/w_320,q_auto,f_auto/038a38ee-81eb-4a80-b163-ab0e20905c22/date-de-sortie-de-la-gato-%C2%AB%C2%A0coastal-blue%C2%A0%C2%BB-hq6020-400.jpg";
+});
+
+document.getElementById("nikeshoes-2").addEventListener("mouseover", () => {
+    document.getElementById("img-display-on").src = "https://static.nike.com/a/images/w_320,q_auto,f_auto/9bc4be31-0822-41c7-ab84-9a90231945ec/date-de-sortie-de-la-gato-%C2%AB%C2%A0coastal-blue%C2%A0%C2%BB-hq6020-400.jpg";
+});
+
+document.getElementById("nikeshoes-3").addEventListener("mouseover", () => {
+    document.getElementById("img-display-on").src = "https://static.nike.com/a/images/w_320,q_auto,f_auto/a5471713-26d6-4325-9135-4d0a3d0a41e3/date-de-sortie-de-la-gato-%C2%AB%C2%A0coastal-blue%C2%A0%C2%BB-hq6020-400.jpg";
+});
+
+document.getElementById("nikeshoes-4").addEventListener("mouseover", () => {
+    document.getElementById("img-display-on").src = "https://static.nike.com/a/images/w_320,q_auto,f_auto/8378ea43-68be-4271-bbba-67d2c2f15565/date-de-sortie-de-la-gato-%C2%AB%C2%A0coastal-blue%C2%A0%C2%BB-hq6020-400.jpg";
+});
+
+document.getElementById("nikeshoes-5").addEventListener("mouseover", () => {
+    document.getElementById("img-display-on").src = "https://static.nike.com/a/images/w_320,q_auto,f_auto/d6053327-9eca-46d4-a1ce-fad45709e8ff/date-de-sortie-de-la-gato-%C2%AB%C2%A0coastal-blue%C2%A0%C2%BB-hq6020-400.jpg";
+});
+
+document.getElementById("nikeshoes-6").addEventListener("mouseover", () => {
+    document.getElementById("img-display-on").src = "https://static.nike.com/a/images/w_320,q_auto,f_auto/01a77d6b-f644-4b83-aef7-7848eaa28533/date-de-sortie-de-la-gato-%C2%AB%C2%A0coastal-blue%C2%A0%C2%BB-hq6020-400.jpg";
+});
